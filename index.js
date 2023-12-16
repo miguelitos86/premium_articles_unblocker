b8be4234b68a72b9d25108a1c9c69a56f8607a6e
gabvader <gabvader@hotmail.com>
2023-12-16 15:18:20 +0100

    modify message only for content script

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-   if (message.action === 'executeBackgroundScript') {
-      const currentTab = await getCurrentTab();
-      const tabId = currentTab?.id;
 
-      await chrome.scripting.executeScript({target: {tabId}, func: modifyDOM}).catch(console.error);
-   }
-});
-
-async function getCurrentTab() {
-   let queryOptions = { active: true, lastFocusedWindow: true };
-   let [tab] = await chrome.tabs.query(queryOptions);
-
-   return tab;
-}
--- popup.js
+++ popup.js
@@ -1,9 +1,4 @@
 document.getElementById('extensionButton').addEventListener('click', () => {
-  chrome.runtime.sendMessage({action: 'executeBackgroundScript'});
+  chrome.runtime.sendMessage({action: 'executeContentScript'});
 });
 
-// Send a message to the content script to execute modifyDOM
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const activeTab = tabs[0];
-  chrome.tabs.sendMessage(activeTab.id, { action: 'executeContentScript' });
-});
