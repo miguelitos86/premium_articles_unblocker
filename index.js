7f28805f6b9993560ef49894aaa36eb660748288
gabvader <gabvader@hotmail.com>
2023-12-16 15:28:00 +0100

    background script

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
-
+// Send a message to the content script to execute modifyDOM
+chrome.browserAction.onClicked.addListener(() => {
+   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+     chrome.tabs.sendMessage(tabs[0].id, { action: 'executeContentScript' });
+   });
+ });
+ 
